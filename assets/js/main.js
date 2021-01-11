41450c49fe23d5423b7ad91fb766028201a5f325
Jiyun-Jeong <61535496+Jiyun-Jeong@users.noreply.github.com>
2021-01-12 02:38:33 +0900

    디자인수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,110 +1,165 @@
 $(document).ready(function () {
-    //cnt4 "droppable"
-    $('#drag1Yes,  #drag2Yes').draggable({
-        revert: 'invalid'
+  var workHei;
+  $(window).on('resize', function () {
+    workHei = $('#work').outerHeight();
+  });
+  $(window).trigger('resize');
+  $('#cnt1 .movebtn').on('click', function () {
+    if ($(this).is('#aboutBtn')) {
+      $('html, body').stop().animate({
+        scrollTop: $('#cnt2').offset().top
       });
+    } else {
+      $('#wrap').css({
+        height: workHei,
+        'overflow': 'hidden'
+      });
+      $('#work').css('visibility', 'visible').parent().stop().animate({
+        left: '-100%'
+      }, function () {
+        $(this).find('.leftPos').css('visibility', 'hidden').attr({
+          'aria-hidden': true,
+          inert: true
+        });
+      });
+    }
+  });
 
-    //드래그를 수락해주는 대상
-	$('#drop1').droppable({
-        accept : '#drag1Yes',        //수락해줄 대상을 지정
-        drop : function () {        //drop이 완료된 후 실행할 실행문
-            $(this).fadeOut().next().text('80%');
-            $('#drag1Yes').hide();
-        }
+  $('#backBtn').on('click', function () {
+    $('#wrap').removeAttr('style');
+    $('.wrap_inner').find('.leftPos').css('visibility', 'visible');
+    $('.wrap_inner').stop().animate({
+      left: 0
+    }, function () {
+      $(this).children('#work').css('visibility', 'hidden').siblings().removeAttr('aria-hidden inert');
     });
-	$('#drop2').droppable({
-        accept : '#drag2Yes',        //수락해줄 대상을 지정
-        drop : function () {        //drop이 완료된 후 실행할 실행문
-            $(this).fadeOut().next().text('80%');
-            $('#drag2Yes').hide();
-        }
-    });
+  });
 
-    //cnt3 "swiper"
-    var storySwiper = new Swiper('#cnt3 .swiper-container', {
-        //direction: 'vertical',
-        pagination: {
-          el: '.swiper-pagination',
-          //type: 'fraction',
-          //clickable: true, //bullet 타입일 경우 버튼 클릭시 이동 가능함
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        //spaceBetween: 10, //각 슬라이더 사이 공간
-        slidesPerView: 'auto',
-        a11y: {
-            prevSlideMessage: '이전 슬라이드 보기',
-            nextSlideMessage: '다음 슬라이드 보기',
-            firstSlideMessage: '첫번째 슬라이드',
-            lastSlideMessage: '마지막 슬라이드',
-          },
-      })
+  //cnt3 "swiper"
+  var storySwiper = new Swiper('#cnt3 .swiper-container', {
+    //direction: 'vertical',
+    pagination: {
+      el: '.swiper-pagination',
+      //type: 'fraction',
+      //clickable: true, //bullet 타입일 경우 버튼 클릭시 이동 가능함
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    //spaceBetween: 10, //각 슬라이더 사이 공간
+    slidesPerView: 'auto',
+    a11y: {
+      prevSlideMessage: '이전 슬라이드 보기',
+      nextSlideMessage: '다음 슬라이드 보기',
+      firstSlideMessage: '첫번째 슬라이드',
+      lastSlideMessage: '마지막 슬라이드',
+    },
+  })
+
+  //cnt4 "droppable"
+  $('#drag1Yes,  #drag2Yes').draggable({
+    revert: 'invalid'
+  });
+
+  //드래그를 수락해주는 대상
+  $('#drop1').droppable({
+    accept: '#drag1Yes', //수락해줄 대상을 지정
+    drop: function () { //drop이 완료된 후 실행할 실행문
+      $(this).fadeOut().next().text('85%');
+      $('#drag1Yes').hide();
+    }
+  });
+  $('#drop2').droppable({
+    accept: '#drag2Yes', //수락해줄 대상을 지정
+    drop: function () { //drop이 완료된 후 실행할 실행문
+      $(this).fadeOut().next().text('80%');
+      $('#drag2Yes').hide();
+    }
+  });
+
+
+  //toggle button
+  var toggleBg = $('.toggleBg');
+  var toggleFg = $('.toggleBg').find('.toggleFg');
+  var left = toggleFg.css('left');
+  if (left == '40px') {
+    toggleBg.css('background', ' #444242');
+    toggleActionStart(toggleGf, 'TO_LEFT');
+  } else if (left == '0px') {
+    toggleBg.css('background', '#ffffff');
+    toggleActionStart(toggleFg, 'TO_RIGHT');
+  }
+  //토글 버튼 이동 모션 함수
+  function toggleActionStart(toggleBtn, LR) {
+    // 0.01초 단위로 실행
+    var intervalID = setInterval(
+      function () {
+        // 버튼 이동
+        var left = parseInt(toggleBtn.css('left'));
+        left += (LR == 'TO_RIGHT') ? 5 : -5;
+        if (left >= 0 && left <= 40) {
+          left += 'px';
+          toggleBtn.css('left', left);
+        }
+      }, 10);
+    setTimeout(function () {
+      clearInterval(intervalID);
+    }, 201);
+  }
+
+
+  출처: https: //dororongju.tistory.com/110 [웹 개발 메모장]
 
     //work page work1 "swiper"
-    var work1Swiper = new Swiper('#cnt3 .swiper-container', {
-        //direction: 'vertical',
-        pagination: {
-          el: '.swiper-pagination',
-          //type: 'fraction',
-          //clickable: true, //bullet 타입일 경우 버튼 클릭시 이동 가능함
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        slidesPerView: 'auto',
-        a11y: {
-            prevSlideMessage: '이전 슬라이드 보기',
-            nextSlideMessage: '다음 슬라이드 보기',
-          },
-      })
-    //work page work2 "swiper"
-    var work2Swiper = new Swiper('#cnt3 .swiper-container', {
-        pagination: {
-          el: '.swiper-pagination',
-          //clickable: true, //bullet 타입일 경우 버튼 클릭시 이동 가능함
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        slidesPerView: 'auto',
-        a11y: {
-            prevSlideMessage: '이전 슬라이드 보기',
-            nextSlideMessage: '다음 슬라이드 보기',
-          },
-      })
-    //work page work3 "swiper"
-    var work3Swiper = new Swiper('#cnt3 .swiper-container', {
-        pagination: {
-          el: '.swiper-pagination',
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        slidesPerView: 'auto',
-        a11y: {
-            prevSlideMessage: '이전 슬라이드 보기',
-            nextSlideMessage: '다음 슬라이드 보기',
-          },
-      })
+    var work1Swiper = new Swiper('#work .gopro .swiper-container', {
+      //direction: 'vertical',
+      pagination: {
+        el: '.swiper-pagination',
+        //type: 'fraction',
+        //clickable: true, //bullet 타입일 경우 버튼 클릭시 이동 가능함
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      slidesPerView: 'auto',
+      a11y: {
+        prevSlideMessage: '이전 슬라이드 보기',
+        nextSlideMessage: '다음 슬라이드 보기',
+      },
+    })
+  //work page work2 "swiper"
+  var work2Swiper = new Swiper('#work .helinox .swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      //clickable: true, //bullet 타입일 경우 버튼 클릭시 이동 가능함
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 'auto',
+    a11y: {
+      prevSlideMessage: '이전 슬라이드 보기',
+      nextSlideMessage: '다음 슬라이드 보기',
+    },
+  })
+  //work page work3 "swiper"
+  var work3Swiper = new Swiper('#work .heyri .swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 'auto',
+    a11y: {
+      prevSlideMessage: '이전 슬라이드 보기',
+      nextSlideMessage: '다음 슬라이드 보기',
+    },
+  })
 
-      //project1-GoPro page "swiper"
-      var gopro7Swiper = new Swiper('#gopro7 .swiper-container', {
-        /* pagination: {
-          el: '.swiper-pagination',
-        }, */
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        slidesPerView: 'auto',
-        a11y: {
-            prevSlideMessage: '이전 슬라이드 보기',
-            nextSlideMessage: '다음 슬라이드 보기',
-          },
-      })
+
 });
