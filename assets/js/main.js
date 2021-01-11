b43f8262b11075fd4a70ba8d816fc74c15393bdc
Jiyun-Jeong <61535496+Jiyun-Jeong@users.noreply.github.com>
2021-01-11 09:16:35 +0900

    heyri cnt3

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
         }
     });
 
-    //cnt4 "swiper"
+    //cnt3 "swiper"
     var storySwiper = new Swiper('#cnt3 .swiper-container', {
         //direction: 'vertical',
         pagination: {
@@ -39,6 +39,72 @@ $(document).ready(function () {
             nextSlideMessage: '다음 슬라이드 보기',
             firstSlideMessage: '첫번째 슬라이드',
             lastSlideMessage: '마지막 슬라이드',
+          },
+      })
+
+    //work page work1 "swiper"
+    var work1Swiper = new Swiper('#cnt3 .swiper-container', {
+        //direction: 'vertical',
+        pagination: {
+          el: '.swiper-pagination',
+          //type: 'fraction',
+          //clickable: true, //bullet 타입일 경우 버튼 클릭시 이동 가능함
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 'auto',
+        a11y: {
+            prevSlideMessage: '이전 슬라이드 보기',
+            nextSlideMessage: '다음 슬라이드 보기',
+          },
+      })
+    //work page work2 "swiper"
+    var work2Swiper = new Swiper('#cnt3 .swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          //clickable: true, //bullet 타입일 경우 버튼 클릭시 이동 가능함
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 'auto',
+        a11y: {
+            prevSlideMessage: '이전 슬라이드 보기',
+            nextSlideMessage: '다음 슬라이드 보기',
+          },
+      })
+    //work page work3 "swiper"
+    var work3Swiper = new Swiper('#cnt3 .swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 'auto',
+        a11y: {
+            prevSlideMessage: '이전 슬라이드 보기',
+            nextSlideMessage: '다음 슬라이드 보기',
+          },
+      })
+
+      //project1-GoPro page "swiper"
+      var gopro7Swiper = new Swiper('#gopro7 .swiper-container', {
+        /* pagination: {
+          el: '.swiper-pagination',
+        }, */
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 'auto',
+        a11y: {
+            prevSlideMessage: '이전 슬라이드 보기',
+            nextSlideMessage: '다음 슬라이드 보기',
           },
       })
 });
